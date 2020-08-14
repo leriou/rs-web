a32451af4bce8fb560abb700a9238ded9b7195d7
leriou <yange1876@hotmail.com>
2020-08-14 20:48:51 +0800

    add redis test

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,13 @@
 mod leetcode;
 
+extern crate redis;
+
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use leetcode::solution::config;
 use leetcode::solution::Solution;
 use listenfd::ListenFd;
+
+use redis::Commands;
 
 async fn logest_prefix() -> impl Responder {
     let c = Solution::longest_common_prefix(vec![
@@ -21,7 +25,20 @@ async fn index(info: web::Path<(u32, String)>) -> impl Responder {
 }
 
 async fn index3() -> impl Responder {
+    fetch_an_integer();
     HttpResponse::Ok().body("Hey there!")
+}
+
+fn fetch_an_integer() -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // throw away the result, just make sure it does not fail
+    let _: () = con.set("my_key", 42)?;
+    // read back the key and return it.  Because the return value
+    // from the function is a result for integer this will automatically
+    // convert into one.
+    con.get("my_key")
 }
 
 #[actix_rt::main]
@@ -33,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             .route("/", web::get().to(logest_prefix))
             .service(index)
             .service(web::scope("/api").configure(config))
-            .service(web::scope("/app").route("/hh", web::get().to(index3)))
+            .service(web::scope("/test").route("/redis", web::get().to(index3)))
     });
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
